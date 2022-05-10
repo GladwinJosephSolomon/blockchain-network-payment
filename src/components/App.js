a53bc616e63d00c918ac1512d996663572e69d90
Gladwin Joseph Solomon B <gladwin1508@gmail.com>
2022-05-10 19:48:39 +0530

    Changes in code

--- src/components/App.js
+++ src/components/App.js
@@ -59,9 +59,6 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             Real Trado
           </a>
@@ -70,11 +67,7 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto" style={{ width: "500px" }}>
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <a>
                   <img src={RealTrado} width="150" />
                 </a>
                 <h1> Rs {this.state.balance} </h1>
